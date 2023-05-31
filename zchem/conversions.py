789ec0cc88dd2f31e90eb41705d5b4afe5f3b5d0
Matthew Zwier <mczwier@gmail.com>
2023-05-30 21:33:31 -0500

    Added a few conversion factors

--- zchem/conversions.py
+++ zchem/conversions.py
@@ -5,3 +5,8 @@ J_per_mole_per_eV = 96485.33212331001
 kJ_per_mole_per_eV = J_per_mole_per_eV / 1000
 
 kJ_per_mole_per_Hartree = 2625.4996394799
+eV_per_Hartree = 27.211386245988
+wavenumbers_per_Hartree = 219474.63136320
+
+wavenumbers_per_eV = wavenumbers_per_Hartree / eV_per_Hartree
+eV_per_wavenumber = 1.0/wavenumbers_per_eV
