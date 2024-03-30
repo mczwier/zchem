883be5a2a68e453b2248587d10fbbe1dbaa5094a
Matthew Zwier <mczwier@gmail.com>
2024-03-29 19:49:51 -0500

    Added a few more conversions/constants

--- zchem/constants.py
+++ zchem/constants.py
@@ -1,1 +1,7 @@
+from scipy import constants as spc
+from . import conversions as zc
+
 MJPERMOL_PER_EV = 1.0 / 10.3642696563
+
+kB_Eh = spc.k / zc.J_per_Hartree
+kB_wavenumbers = spc.k / zc.J_per_eV * zc.wavenumbers_per_eV
--- zchem/conversions.py
+++ zchem/conversions.py
@@ -4,7 +4,9 @@ eV_per_J = 1.0/J_per_eV
 J_per_mole_per_eV = 96485.33212331001
 kJ_per_mole_per_eV = J_per_mole_per_eV / 1000
 
+J_per_Hartree = 4.35974472221e-18
 kJ_per_mole_per_Hartree = 2625.4996394799
+J_per_mole_per_Hartree = kJ_per_mole_per_Hartree * 1000
 eV_per_Hartree = 27.21138624599
 wavenumbers_per_Hartree = 219474.6313632
 
